7271b2ac009ee07b7504f0c98731ba007f35713c
Brian Mancini <bmancini@gmail.com>
2014-11-05 06:52:16 -0800

    Added back project list

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -9,12 +9,16 @@ var deployerApp = angular.module('deployerApp', [
 
 deployerApp.config(['$routeProvider',
   function($routeProvider) {
-    $routeProvider.
-    when('/projects/:projectId', {
+    $routeProvider
+    .when('/projects', {
+      templateUrl: 'app/partials/project-list.html',
+      controller: 'ProjectListCtrl'
+    })
+    .when('/projects/:projectId', {
       templateUrl: 'app/partials/project-details.html',
       controller: 'ProjectDetailsCtrl'
-    }).
-    otherwise({
-      redirectTo: '/projects/31502'
+    })
+    .otherwise({
+      redirectTo: '/projects/'
     });
   }]);
