7f583c896e57ad3dc53d39d5bc12cae7bb63d0ae
Brian Mancini <bmancini@gmail.com>
2014-11-11 14:37:19 -0800

    Fixed redeploy command to fully execute

--- src/server/tasks/redeploytask.js
+++ src/server/tasks/redeploytask.js
@@ -34,56 +34,57 @@ RedeployTask.prototype.start = function start() {
 
   // find the newest template
   return Q.fcall(function() {
-    debug('startRedeploy: finding newest template');
+    debug('redeploy: finding newest template');
 
     return skytap.projects.templates({ project_id: project_id })  
     .then(function(templates) {      
       scope.template = _.max(templates, function(template) {
         return template.id;
       });  
-      debug('startRedeploy: found newest template %s', scope.template.id);
+      debug('redeploy: found newest template %s', scope.template.id);
     })    
   })  
 
   // retrieve the old environment 
   .then(function() {      
-    debug('startRedeploy: finding the environment');
+    debug('redeploy: finding the environment');
 
     return skytap.environments.get({ configuration_id: configuration_id })
     .then(function(oldEnv) {
-      debug('startRedeploy: old environment %s', oldEnv.id);
+      debug('redeploy: old environment %s', oldEnv.id);
       scope.oldEnv = oldEnv;
     });  
   })
 
   // rename the old environment
   .then(function() {      
-    debug('startRedeploy: renaming old environment');
+    debug('redeploy: renaming old environment');
 
     return skytap.environments.update({ configuration_id: scope.oldEnv.id, name: scope.oldEnv.name + ' - DECOMMISIONING' })    
     .then(function(oldEnv) {
-      debug('startRedeploy: renamed old environment to %s', oldEnv.name);
+      debug('redeploy: renamed old environment to %s', oldEnv.name);
       scope.oldEnv = oldEnv;
     });
   })
 
   // stop the old environment
   .then(function() {
-    debug('stopRedeploy: stopping old environment');
+    debug('redeploy: suspending old environment');
 
-    return skytap.environment.stop({ configuration_id: scope.oldEnv.id })
+    return skytap.environments.suspend({ configuration_id: scope.oldEnv.id })
     .then(function() {
-      return skytap.environment.waitForState({ configuration_id: scope.oldEnv.id, runstate: 'stopped' });
+      debug('redeploy: waitigin for suspended state');
+      return skytap.environments.waitForState({ configuration_id: scope.oldEnv.id, runstate: 'suspended' });
     })
     .then(function(oldEnv) {
-      debug('stopRedeploy: old environment stopped')
+      debug('redeploy: old environment suspended')
       scope.oldEnv = oldEnv;
     });
   })
 
   // detach public ip addresses
   .then(function() {
-    debug('startRedeploy: detaching public ip addresses');
+    debug('redeploy: detaching public ip addresses');
 
     // get the ip addresses to detach
     scope.detachIps = scope.oldEnv.vms.map(function(vm) {
@@ -101,22 +102,22 @@ RedeployTask.prototype.start = function start() {
     // perform the detaches
     return Q.all(scope.detachIps.map(function (detachIp) {          
       if(detachIp.ip) {
-        debug('startRedeploy: detaching: %j', detachIp);
+        debug('redeploy: detaching: %j', detachIp);
         return skytap.ips.detach(detachIp);
       }        
       return null;          
     }))
     .then(function() {
-      debug('startRedeploy: all public ips detached');
+      debug('redeploy: all public ips detached');
     });    
   })
 
   // create the new environment
   .then(function() {      
-    debug('startRedeploy: creating new environment');
+    debug('redeploy: creating new environment');
     return skytap.environments.create({ template_id: scope.template.id })
     .then(function(newEnv) {
-      debug('startRedeploy: new environment created %s', newEnv.id);
+      debug('redeploy: new environment created %s', newEnv.id);
       scope.newEnv = newEnv;
     });
   })
@@ -124,26 +125,26 @@ RedeployTask.prototype.start = function start() {
 
   // add new environment to project
   .then(function() {      
-    debug('startRedeploy: adding environment to project %s', project_id);
+    debug('redeploy: adding environment to project %s', project_id);
     return skytap.projects.addEnvironment({ 
       project_id: project_id,
       configuration_id: scope.newEnv.id    
     })
     .then(function() {
-      debug('startRedeploy: added environment to project');
+      debug('redeploy: added environment to project');
     });
   })  
 
   // start environemnt
   .then(function() {
-    debug('startRedeploy: start new envionrment');
+    debug('redeploy: start new envionrment');
 
     return skytap.environments.update({
       configuration_id: scope.newEnv.id,
       runstate: 'running'
     })
     .then(function(environment) {
-      debug('startRedeploy: waiting for run state');
+      debug('redeploy: waiting for running state');
 
       return skytap.environments.waitForState({
         configuration_id: environment.id,
@@ -151,22 +152,98 @@ RedeployTask.prototype.start = function start() {
       });
     })
     .then(function(environment) {
-      debug('startRedeploy: new environment has been started')
+      debug('redeploy: new environment has been started')
       scope.newEnv = environment;
     });
   })
 
+
   // start installation
   .then(function() {
-    debug('startRedeploy: start installation')
+    debug('redeploy: start installation')
+              
+    var vm = scope.newEnv.vms[0]
+      , ip_address = vm.interfaces[0].nat_addresses.vpn_nat_addresses[0].ip_address
+      , installUrl = 'http://' + ip_address + ':8080/api/installer'
+      , statusUrl  = 'http://' + ip_address + ':8080/api/status';
 
+    console.log(installUrl);
+    console.log(statusUrl);
+    return Q.fcall(function() {
+      debug('redeploy: wait for install service');
 
+      var deferred = Q.defer();
+      var poll = function() {
+        request(statusUrl, function(err, response, body) {
+          if(err || response.statusCode !== 200) {
+            debug('redeploy: wait for install service');
+            setTimeout(poll, 15000);
+          } else {
+            deferred.resolve(body);
+          }
+        });
+      }
+      poll();
+      return deferred.promise;
+    })
+        
+    // start installation
+    .then(function() {
+      debug('redeploy: installing');
+      var deferred = Q.defer()
 
+      request(installUrl, function(error, response, body) {
+        if(!error && response.statusCode === 200) {            
+          deferred.resolve(ip_address);
+        } else {            
+          deferred.reject(response);
+        }
+      });
+
+      return deferred.promise;
+    })
+
+    // wait for installation to complete  
+    .then(function() {
+      debug('redeploy: wait for installation');
+
+      var deferred = Q.defer();
+      var poll = function() {        
+        
+        request(statusUrl, function(err, response, body) {          
+          if(!err && response.statusCode === 200) {
+
+            // add logic for checking status
+            if(true) {
+              deferred.resolve();
+            } 
+
+            // if not in completed status continue polling
+            else {
+              debug('redeploy: wait for installation');
+              setTimeout(poll, 15000);
+            }
+
+          } else {
+            deferred.reject({ err: err, response: response, body: body });
+          }
+        });        
+      }
+
+      poll();
+      return deferred.promise;
+    })
+
+    // signal completion for installation
+    .then(function() {
+      debug('redeploy: installations complete');
+    });
   })
+
 
   // attach public ip addresses
   .then(function() {
-    debug('startRedeploy: attach public ip addresses');
+    debug('redeploy: attach public ip addresses');
 
     // get the ip addresses to attach
     scope.attachIps = scope.newEnv.vms.map(function(vm, idx) {
@@ -181,18 +258,18 @@ RedeployTask.prototype.start = function start() {
     // attach IPs  
     return Q.all(scope.attachIps.map(function(attachIp) {    
       if(attachIp.ip) {
-        debug('startRedeploy: attaching: %j', attachIp);
+        debug('redeploy: attaching: %j', attachIp);
         return skytap.ips.attach(attachIp);
       }
       return null;
     }))  
     .then(function() {
-      debug('startRedeploy: all public ips attached');
+      debug('redeploy: all public ips attached');
     });
   })
 
   // remove vpn connection
   .then(function() {
-    debug('startRedeploy: removing vpn connection');
+    debug('redeploy: removing vpn connection');
   });
 }
