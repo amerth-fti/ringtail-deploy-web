56f3f5fd6f2823d7a1e4ef61b6610b8cfb2e8c1d
jakebrown58 <jake.brown.pa@gmail.com>
2016-05-20 12:54:33 -0400

    Support file listing in the smb-browser
    SMB-browser now will list out the files when you've selected a branch
    and a build.

--- src/server/services/browsers/smb-browser.js
+++ src/server/services/browsers/smb-browser.js
@@ -1,4 +1,5 @@
 var fs    = require('fs')
+  , debug = require('debug')('deployer-browsers-smb-browser')
   , path  = require('path')
   , _     = require('underscore')
   , Q     = require('q')
@@ -37,6 +38,18 @@ SmbBrowser.prototype.builds = function builds(branch, next) {
   return SmbBrowser.listDirs(branchPath).nodeify(next);
 };
 
+/**
+ * Retrieves the list of builds for a branch as a list of strings
+ *
+ * @param {string} branch - the branch to retrieve builds for
+ * @param {function} [next] - node callback
+ * @return {promise}
+ */
+SmbBrowser.prototype.files = function files(branch, next) {
+  var branchPath = path.join(this.smbPath, branch);
+  return SmbBrowser.listFiles(branchPath).nodeify(next);
+};
+
 
 /**
  * Lists the directories in the path provided
@@ -46,13 +59,49 @@ SmbBrowser.prototype.builds = function builds(branch, next) {
  * @return {promise} resolves to an array of directory names
  */
 SmbBrowser.listDirs = function listDirs(dir, next) {  
+  var foldersOnly = function(stat, file) {
+    return stat.isDirectory() ? file : null;
+  },
+    cfg = {
+      dir: dir,
+      filterFn: foldersOnly
+    };
+
+  return this.filteredListContents(cfg, next);
+};
+
+/**
+ * Lists the files in the path provided
+ * 
+ * @param {string} dir - the root path to check
+ * @param {function} [next] - node callback
+ * @return {promise} resolves to an array of directory names
+ */
+SmbBrowser.listFiles = function listFiles(dir, next) {
+  var foldersOnly = function(stat, file) {
+    return stat.isDirectory() ? null : file;
+  },
+    cfg = {
+      dir: dir,
+      filterFn: foldersOnly
+    };
+
+  return this.filteredListContents(cfg, next);
+};
+
+SmbBrowser.filteredListContents = function filteredListContents(cfg, next) {
+  var dir = cfg.dir,
+    filterFn = cfg.filterFn;
+
   return Q
     .nfcall(fs.readdir, dir)    
     .then(function(files) {        
       return Q.all(files.map(function(file) {
-        var fullPath = path.join(dir, file);        
+        var fullPath = path.join(dir, file);    
         return Q.nfcall(fs.stat, fullPath).then(function(stat) {
-          return stat.isDirectory() ? file : null;
+          var x = filterFn(stat, file);
+          debug('in here %s', x);
+          return x;
         });
       }));
     })
@@ -63,3 +112,4 @@ SmbBrowser.listDirs = function listDirs(dir, next) {
     })
     .nodeify(next);
 };
+
