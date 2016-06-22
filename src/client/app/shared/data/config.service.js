9f36737a0be7254a149d492f83f2eb60339d7ad5
Jason McFall <jsnmcdev@gmail.com>
2016-06-22 15:20:03 -0400

    Get the lit keys back from the DB and select the keys that are selected.  Add logic to handle selecting the whole node branch with all keys are selected.  Handle the clicking event on the node when not all children are selected ( retain the one that are set in the DB already ). Adjust the key name width.

--- src/client/app/shared/data/config.service.js
+++ src/client/app/shared/data/config.service.js
@@ -12,10 +12,11 @@
       'api/configs/:configId',
       { configId: '@configId' },
       {
-        findByEnv: { method: 'GET',  url: 'api/envs/:envId/configs', isArray: true },
-        launchKeys: { method: 'GET',  url: 'api/envs/:envId/branches/:branch/launchKeys', isArray: true },
-        sendLaunchKeys: { method: 'PUT',  url: 'api/envs/sendLaunchKeys'},
-        update   : { method: 'PUT',  url: 'api/configs/:configId' }
+          findByEnv: { method: 'GET',  url: 'api/envs/:envId/configs', isArray: true },
+          launchKeys: { method: 'GET',  url: 'api/envs/:envId/branches/:branch/launchKeys', isArray: true },
+          sendLaunchKeys: { method: 'PUT',  url: 'api/envs/sendLaunchKeys'},
+          update   : { method: 'PUT',  url: 'api/configs/:configId' },
+          litKeys: { method: 'GET',  url: 'api/envs/:envId/branches/:branch/litKeys', isArray: true },
       }
     );
   }
