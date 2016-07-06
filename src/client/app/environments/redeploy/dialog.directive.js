dc9824cb0b9e5554ae74e3e8de0f5de4d975bfad
jakebrown58 <jake.brown.pa@gmail.com>
2016-07-06 14:02:13 -0400

    Fixes issues with selection

--- src/client/app/environments/redeploy/dialog.directive.js
+++ src/client/app/environments/redeploy/dialog.directive.js
@@ -382,7 +382,7 @@
       listOfKeys.forEach(function(keyItemDetail) {
         var isChecked = false;
         if (vm.litKeys.indexOf(keyItemDetail.FeatureKey) != -1) {
-                    isChecked = true;
+          isChecked = true;
         }
         
         filterLevelItemRoot.children.push({
@@ -398,13 +398,9 @@
           });
         });
             
-        filterLevelItemRoot.children.forEach(function (child) {
-          if (child.isSelected != true) {
-            rootItemChecked = false;
-            return;
-          }
-          rootItemChecked = true;
-        });
+        rootItemChecked = _.filter(filterLevelItemRoot.children, function(child) {
+          return child.isSelected === false;
+        }).length === 0;
             
         // Check the parent root node if necessary
         if (rootItemChecked) {
