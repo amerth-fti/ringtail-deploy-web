f3e38e5b9220fb0efc7fc23f6ddec8a6ee0c90ea
jakebrown58 <jake.brown.pa@gmail.com>
2016-08-02 11:16:30 -0400

    fixing lint.

--- test/server/services/jobrunner.js
+++ test/server/services/jobrunner.js
@@ -10,7 +10,7 @@ describe('Job Runner', function() {
   describe('.arrayifyDetails ', function() {
     describe('when no results yet', function() {
       it('should create an empty array', function() {
-        var result = runner.convertdetailstoarray("");
+        var result = runner.convertdetailstoarray('');
         expect(result).to.be.instanceOf(Array);
         expect(result.length).to.equal(0);
       });
@@ -18,7 +18,7 @@ describe('Job Runner', function() {
 
     describe('when some results', function() {
       it('should create a non-empty array', function() {
-        var content = "<html><p>start1</p><p>finish1</p><p>-----------</p><p>start2</p></html>";
+        var content = '<html><p>start1</p><p>finish1</p><p>-----------</p><p>start2</p></html>';
         var result = runner.convertdetailstoarray(content);
         console.log(result);
         expect(result).to.be.instanceOf(Array);
